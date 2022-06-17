710f301be3af36df1f4c0fdcc2adf3f125c0fb2a
Israel Guillermo <learsi.guil2010@gmail.com>
2022-06-17 14:32:04 -0400

    Pequeños ajustes

--- js/democaja.js
+++ js/democaja.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
     var total = 0.00;
     $("#cafe").click(function() {
-        var markup = "<tr><td>Café</td><td>2.50</td><td>1</td><td>12.50</td></tr>";
+        var markup = "<tr><td>Café</td><td>2.50</td><td>1</td><td>2.50</td></tr>";
         $("#lista tbody").append(markup);
         total += parseFloat("2.50");
         $("#total").val(total.toFixed(2));
     });
 
     $("#latte").click(function() {
-        var markup = "<tr><td>Latte</td><td>4.50</td><td>1</td><td>14.50</td></tr>";
+        var markup = "<tr><td>Latte</td><td>4.50</td><td>1</td><td>4.50</td></tr>";
         $("#lista tbody").append(markup);
         total += parseFloat("4.50");
         $("#total").val(total.toFixed(2));
     });
 
     $("#choco").click(function() {
-        var markup = "<tr><td>Chocolate</td><td>3.50</td><td>1</td><td>13.50</td></tr>";
+        var markup = "<tr><td>Chocolate</td><td>3.50</td><td>1</td><td>3.50</td></tr>";
         $("#lista tbody").append(markup);
         total += parseFloat("3.50");
         $("#total").val(total.toFixed(2));
